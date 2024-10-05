986466e310c3ec58db0243edf05da42dbcf03be1
Yousinator <y.omusabeh@gmail.com>
2024-10-06 01:23:27 +0300

    sigh

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Nasa-SpaceApps-Amman.github.io/", // Make sure this matches your GitHub Pages URL
+  base: "/", // Make sure this matches your GitHub Pages URL
 });
