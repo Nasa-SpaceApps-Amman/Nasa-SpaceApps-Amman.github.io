65214a88ddc4f19af9cfa59abb100c5e22e57daf
Yousinator <y.omusabeh@gmail.com>
2024-10-06 00:58:37 +0300

    redid the whole tech stack ++

--- src/db.js
+++ src/db.js
@@ -1,8 +1,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 // Initialize Supabase client
-const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
-const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 // Reusable function to insert data
